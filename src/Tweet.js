d90017005c0af5cec916980341e4acafbed91c68
ashutoshdumiyan <ashutosh.dumiyan@gmail.com>
2020-10-05 22:12:53 +0530

    ui polishing

--- src/Tweet.js
+++ src/Tweet.js
@@ -4,32 +4,36 @@ class Tweet extends Component {
   render() {
     const tweet = this.props.data;
     return (
-      <div className="tweet">
-        <div className="head">
-          <img
-            src={tweet.user.profile_image_url_https}
-            className="profilepic"
-            alt="Profile"
-          />
-          <div
-            className="user"
-            style={{ display: "flex", flexDirection: "column" }}
-          >
-            <div>
-              <strong>{tweet.user.name}</strong>
+      <div className="card w-75">
+        <div className="card-header">
+          <div className="head">
+            <img
+              src={tweet.user.profile_image_url_https}
+              className="profilepic"
+              alt="Profile"
+            />
+            <div
+              className="user"
+              style={{ display: "flex", flexDirection: "column" }}
+            >
+              <div>
+                <strong>{tweet.user.name}</strong>
+              </div>
+              <div>@{tweet.user.screen_name}</div>
             </div>
-            <div>@{tweet.user.screen_name}</div>
           </div>
         </div>
-        <div className="body">{tweet.text}</div>
-        <div className="footer">
+        <div className="card-body">
+          <p className="card-text">{tweet.text}</p>
+        </div>
+        <div className="card-footer text-muted">
           <a
-            className="btn btn-primary"
+            className="btn btn-outline-primary"
             target="_blank"
             rel="noopener noreferrer"
             href={`https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`}
           >
-            View Tweet
+            Go to Tweet &#8599;
           </a>
         </div>
       </div>

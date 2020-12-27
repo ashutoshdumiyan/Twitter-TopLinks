231091c25aa0946d27bd9f90939af5e7284e5e87
Ashutosh Dumiyan <33063623+ashutoshdumiyan@users.noreply.github.com>
2020-12-27 16:53:30 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,11 @@
 const twitter = {
-  consumerkey: "X9Nw89uflL06qC8E7o0HycAJO",
-  consumersecret: "A6LRxU01UfPwmTTBne1eN2eenN3HQ338pgx7u2HkjqqFIuiiDD",
+  consumerkey: "CONSUMER_KEY",
+  consumersecret: "CONSUMER_SEXRET_KEY",
 };
 
 const mongodb = {
   dbURI:
-    "mongodb+srv://ashutosh:twitterapp@cluster0.fxxjl.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    "mongodb+srv://dbname:dbpwd@cluster0.fxxjl.mongodb.net/<dbname>?retryWrites=true&w=majority",
 };
 
 const session = {
